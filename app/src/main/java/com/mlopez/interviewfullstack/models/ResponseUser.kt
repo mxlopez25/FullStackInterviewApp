b189646fd88929f7fdb57c7a8f35a803f643ce38
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-26 00:35:36 -0400

    ResponseUser updated

--- app/src/main/java/com/mlopez/interviewfullstack/models/ResponseUser.kt
+++ app/src/main/java/com/mlopez/interviewfullstack/models/ResponseUser.kt
@@ -2,4 +2,4 @@ package com.mlopez.interviewfullstack.models
 
 import com.google.gson.annotations.SerializedName
 
-data class ResponseUser( @SerializedName("users") val user: List<Users> )
+data class ResponseUser( @SerializedName("users") val user: MutableList<Users> )
