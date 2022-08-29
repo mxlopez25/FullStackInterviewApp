aa6e7c57e6e1d8afd38590e8b03ba6bcb17eb12b
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-29 16:30:08 -0400

    Added Constant Object to project

---
+++ app/src/main/java/com/mlopez/interviewfullstack/utils/Constants.kt
@@ -0,0 +1,23 @@
+package com.mlopez.interviewfullstack.utils
+
+class Constants {
+    companion object {
+        val USER_FIRSTNAME = "Firstname: "
+        val USER_FIRSTNAME_LABEL = "user_firstname_label"
+        val USER_FIRSTNAME_VALUE = "user_firstname_value"
+
+        val USER_LASTNAME = "Lastname: "
+        val USER_LASTNAME_LABEL = "user_lastname_label"
+        val USER_LASTNAME_VALUE = "user_lastname_value"
+
+        val USER_EMAIL = "Email: "
+        val USER_EMAIL_LABEL = "user_email_label"
+        val USER_EMAIL_VALUE = "user_email_value"
+
+        val USER_USERNAME = "Username: "
+        val USER_USERNAME_LABEL = "user_username_label"
+        val USER_USERNAME_VALUE = "user_username_value"
+
+
+    }
+}
