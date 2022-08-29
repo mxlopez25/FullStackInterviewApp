e720ea0e9e5662e6a684bcf5858678da7932a763
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-29 01:22:55 -0400

    User and Product fragment updated and implemented

--- app/src/main/java/com/mlopez/interviewfullstack/UserFragment.kt
+++ app/src/main/java/com/mlopez/interviewfullstack/UserFragment.kt
@@ -5,55 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mlopez.interviewfullstack.adapters.UserListAdapter
+import com.mlopez.interviewfullstack.databinding.FragmentUserBinding
+import com.mlopez.interviewfullstack.repositories.ProductApiRepository
+import com.mlopez.interviewfullstack.repositories.UserApiRepository
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UserFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UserFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentUserBinding
+    private lateinit var sharedViewModel: SharedViewModel
+    private lateinit var sharedViewModelFactory: SharedViewModelFactory
+    private lateinit var userRepo: UserApiRepository
+    private lateinit var productRepo: ProductApiRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false)
+        binding = FragmentUserBinding.inflate(inflater, container, false)
+        userRepo = UserApiRepository()
+        productRepo = ProductApiRepository()
+
+        sharedViewModelFactory = SharedViewModelFactory(userRepo, productRepo)
+
+        sharedViewModel = ViewModelProvider(requireActivity(), sharedViewModelFactory).get(SharedViewModel::class.java)
+
+        sharedViewModel.users.observe(viewLifecycleOwner) {
+            binding.rvUserList.adapter = UserListAdapter(it)
+            binding.rvUserList.layoutManager = LinearLayoutManager(context)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
