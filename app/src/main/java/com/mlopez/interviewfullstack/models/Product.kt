d843aa7ac87c886e742a064e82f71bfad857c5d8
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-30 18:02:15 -0400

    Update in models fields

--- app/src/main/java/com/mlopez/interviewfullstack/models/Product.kt
+++ app/src/main/java/com/mlopez/interviewfullstack/models/Product.kt
@@ -1,8 +1,8 @@
 package com.mlopez.interviewfullstack.models
 
 data class Product(
-    val id: Int,
-    val name: String? = null,
-    val sku: String? = null,
-    val price: Double = 0.0
+    var id: Int,
+    var name: String? = null,
+    var sku: String? = null,
+    var price: Double = 0.0
 )
--- app/src/main/java/com/mlopez/interviewfullstack/models/Users.kt
+++ app/src/main/java/com/mlopez/interviewfullstack/models/Users.kt
@@ -3,10 +3,10 @@ package com.mlopez.interviewfullstack.models
 import com.google.gson.annotations.SerializedName
 
 data class Users (
-    @SerializedName("id") val id: Int? = null,
-    @SerializedName("firstName") val firstName: String? = null,
-    @SerializedName("lastName") val lastName: String? = null,
-    @SerializedName("email") val email: String? = null,
-    @SerializedName("username") val username: String? = null,
-    @SerializedName("password") val password: String? = null
+    @SerializedName("id") var id: Int? = null,
+    @SerializedName("firstName") var firstName: String? = null,
+    @SerializedName("lastName") var lastName: String? = null,
+    @SerializedName("email") var email: String? = null,
+    @SerializedName("username") var username: String? = null,
+    @SerializedName("password") var password: String? = null
         )
