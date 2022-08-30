501520974fd572b96d1cf3dc09f9e4fdd7c34f13
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-29 23:41:16 -0400

    User Details activity Extras added

--- app/src/main/java/com/mlopez/interviewfullstack/adapters/UserListAdapter.kt
+++ app/src/main/java/com/mlopez/interviewfullstack/adapters/UserListAdapter.kt
@@ -1,15 +1,19 @@
 package com.mlopez.interviewfullstack.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.mlopez.interviewfullstack.DetailsActivity
 import com.mlopez.interviewfullstack.R
 import com.mlopez.interviewfullstack.models.Users
+import com.mlopez.interviewfullstack.utils.Constants
 
 class UserListAdapter(private val list: List<Users>): RecyclerView.Adapter<UserListAdapter.ViewHolder>() {
     private lateinit var ctx: Context
@@ -24,7 +28,24 @@ class UserListAdapter(private val list: List<Users>): RecyclerView.Adapter<UserL
         holder.tvName.text = "${user.firstName} ${user.lastName}"
 
         holder.clContainer.setOnClickListener {
-            Toast.makeText(ctx, "${user.firstName} ${user.lastName}", Toast.LENGTH_SHORT).show()
+            val intent = Intent(ctx, DetailsActivity::class.java)
+
+            intent.putExtra(Constants.USER_ID_LABEL, Constants.USER_ID)
+            intent.putExtra(Constants.USER_ID_VALUE, user.id)
+
+            intent.putExtra(Constants.USER_FIRSTNAME_LABEL , Constants.USER_FIRSTNAME)
+            intent.putExtra(Constants.USER_FIRSTNAME_VALUE , user.firstName)
+
+            intent.putExtra(Constants.USER_LASTNAME_LABEL , Constants.USER_LASTNAME)
+            intent.putExtra(Constants.USER_LASTNAME_VALUE , user.lastName)
+
+            intent.putExtra(Constants.USER_EMAIL_LABEL , Constants.USER_EMAIL)
+            intent.putExtra(Constants.USER_EMAIL_VALUE , user.email)
+
+            intent.putExtra(Constants.USER_USERNAME_LABEL , Constants.USER_USERNAME)
+            intent.putExtra(Constants.USER_USERNAME_VALUE , user.username)
+
+            startActivity(ctx, intent, null)
         }
     }
 
