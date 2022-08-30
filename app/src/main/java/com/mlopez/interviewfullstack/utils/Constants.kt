71656129861533fae246aedae4477ee291899df6
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-29 23:39:13 -0400

    Product Constants added

--- app/src/main/java/com/mlopez/interviewfullstack/utils/Constants.kt
+++ app/src/main/java/com/mlopez/interviewfullstack/utils/Constants.kt
@@ -2,6 +2,10 @@ package com.mlopez.interviewfullstack.utils
 
 class Constants {
     companion object {
+        val USER_ID = "ID: "
+        val USER_ID_LABEL = "user_id_label"
+        val USER_ID_VALUE = "user_id_value"
+
         val USER_FIRSTNAME = "Firstname: "
         val USER_FIRSTNAME_LABEL = "user_firstname_label"
         val USER_FIRSTNAME_VALUE = "user_firstname_value"
@@ -18,6 +22,20 @@ class Constants {
         val USER_USERNAME_LABEL = "user_username_label"
         val USER_USERNAME_VALUE = "user_username_value"
 
+        val PRODUCT_ID = "Product ID: "
+        val PRODUCT_ID_LABEL = "product_id_label"
+        val PRODUCT_ID_VALUE = "product_id_value"
 
+        val PRODUCT_NAME = "Product Name:"
+        val PRODUCT_NAME_LABEL = "product_name_label"
+        val PRODUCT_NAME_VALUE = "product_name_value"
+
+        val PRODUCT_SKU = "Product SKU: "
+        val PRODUCT_SKU_LABEL = "product_sku_label"
+        val PRODUCT_SKU_VALUE = "product_sku_label"
+
+        val PRODUCT_PRICE = "Product Price: "
+        val PRODUCT_PRICE_LABEL = "product_price_label"
+        val PRODUCT_PRICE_VALUE = "product_price_value"
     }
 }
